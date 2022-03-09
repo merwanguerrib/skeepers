e8a5b965f46ff3ba37f2ad0332ed5d2074749899
Merwan Guerrib <merwan.guerrib@capgemini.com>
2022-03-09 18:53:23 +0100

    Destructuring API response data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,26 @@
 import React, { useState, useEffect, Fragment } from 'react'
-import { UserType } from '../commonTypes/user';
+import { userType } from '../commonTypes/userType';
 import UserCard from './UserCard';
 const ENDPOINT_RANDOM_USERS = 'https://randomuser.me/api/?results=20';
 
 
 function Home():JSX.Element {
-  const [users, setUsers] = useState<UserType[]>([])
+  const [users, setUsers] = useState<userType[]>([])
 
   const fetchUsers = async () => {
     if(users.length === 0){
       try {
         const response = await fetch(ENDPOINT_RANDOM_USERS)
         const usersArr = await response.json()
-         setUsers(usersArr.results)
+        const results = usersArr.results.map((result: userType) => {
+          let user = {
+          name : result.name,
+          location : result.location,
+          email: result.email
+          } as userType
+          return user
+        })  
+         setUsers(results)
        } catch (error) {
          console.error(error)
        }
@@ -26,11 +34,11 @@ function Home():JSX.Element {
   return (
     <Fragment>
       {
-      users.map((user:UserType) => { 
+       users.map((user:userType,key:number) => { 
         return(
-        <UserCard {...user} key={user.login.uuid} />
+        <UserCard {...user} key={key} />
         )
-      })
+      }) 
       }
     </Fragment>
   )
