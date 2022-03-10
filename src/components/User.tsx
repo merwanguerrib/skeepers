c0a961c6e2997724ea8b37f67d4a3b4e0e013f76
Merwan Guerrib <merwan.guerrib@capgemini.com>
2022-03-10 18:44:03 +0100

    Edit and view user data

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,159 +1,229 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { useLocation } from 'react-router'
-import { userType } from '../commonTypes/userType';
+import React, { Fragment, useState } from "react";
+import { useLocation } from "react-router";
+import { userType } from "../commonTypes/userType";
 
 export const User = () => {
   const location = useLocation();
   const user = location.state as userType;
-  const [editMode, setEditMode] = useState(false)
+  const [editMode, setEditMode] = useState(false);
+  const [userDetails, setUserDetails] = useState(user);
+
+  const handleChange = (e: { target: HTMLInputElement }) => {
+    setUserDetails(
+      (prevUser): userType => ({
+        name: {
+          ...prevUser.name,
+          [e.target.name]: e.target.value,
+        },
+        location: {
+          ...prevUser.location,
+          [e.target.name]: e.target.value,
+          street: {
+            ...prevUser.location.street,
+            [e.target.name]: e.target.value,
+          },
+        },
+        email: e.target.name !== "email" ? prevUser.email : e.target.value,
+      })
+    );
+  };
 
   const handleSave = () => {
-    setEditMode(false)
-  }
+    setEditMode(false);
+  };
 
   const editModeTrue = () => {
     return (
-  <div className="">
-    <div className="field">
-        <label className="label">Firstname</label>
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="text" name="first" placeholder={user.name.first} value={user.name.first} />
-          <span className="icon is-small is-left">
-          <i className="fas fa-id-card" />
-          </span>
-        </p>
-    </div>
-    <div className="field">
-        <label className="label">Lastname</label>
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="text" name="last" placeholder={user.name.last} value={user.name.last} />
-          <span className="icon is-small is-left">
-          <i className="fas fa-id-card" />
-          </span>
-        </p>
-    </div>
-    <div className="field">
-        <label className="label">Email</label>
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="email" name="email" placeholder={user.email} value={user.email} />
-          <span className="icon is-small is-left">
-          <i className="fas fa-envelope" />
-          </span>
-        </p>
-    </div>
-    <div className="field">
-        <label className="label">Number</label>
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="number" name="number" value={user.location.street.number} />
-          <span className="icon is-small is-left">
-          <i className="fas fa-map-location-dot" />
-          </span>
-        </p>
-    </div>
-    <div className="field">
-        <label className="label">Street</label>
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="text" name="street" placeholder={user.location.street.name} value={user.location.street.name} />
-          <span className="icon is-small is-left">
-          <i className="fas fa-map-location-dot" />
-          </span>
-        </p>
-    </div>
-    <div className="field">
-        <label className="label">City</label>
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="text" name="city" placeholder={user.location.city} value={user.location.city} />
-          <span className="icon is-small is-left">
-          <i className="fas fa-map-location-dot" />
-          </span>
-        </p>
-    </div>
-    <div className="field">
-        <label className="label">Country</label>
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="text" name="country" placeholder={user.location.country} value={user.location.country} />
-          <span className="icon is-small is-left">
-          <i className="fas fa-earth-europe" />
-          </span>
-        </p>
-    </div>
-  </div>
-
-    )
-  } 
+      <div className="">
+        <div className="field">
+          <label className="label">Firstname</label>
+          <p className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="text"
+              name="first"
+              placeholder={userDetails.name.first}
+              value={userDetails.name.first}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left paddingLeft">
+              <i className="fas fa-id-card" />
+            </span>
+          </p>
+        </div>
+        <div className="field">
+          <label className="label">Lastname</label>
+          <p className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="text"
+              name="last"
+              placeholder={userDetails.name.last}
+              value={userDetails.name.last}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left paddingLeft">
+              <i className="fas fa-id-card" />
+            </span>
+          </p>
+        </div>
+        <div className="field">
+          <label className="label">Email</label>
+          <p className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="email"
+              name="email"
+              placeholder={userDetails.email}
+              value={userDetails.email}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left paddingLeft">
+              <i className="fas fa-envelope" />
+            </span>
+          </p>
+        </div>
+        <div className="field">
+          <label className="label">Number</label>
+          <p className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="number"
+              name="number"
+              value={userDetails.location.street.number}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left paddingLeft">
+              <i className="fas fa-home" />
+            </span>
+          </p>
+        </div>
+        <div className="field">
+          <label className="label">Street</label>
+          <p className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="text"
+              name="name"
+              placeholder={userDetails.location.street.name}
+              value={userDetails.location.street.name}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left paddingLeft">
+              <i className="fas fa-home" />
+            </span>
+          </p>
+        </div>
+        <div className="field">
+          <label className="label">City</label>
+          <p className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="text"
+              name="city"
+              placeholder={userDetails.location.city}
+              value={userDetails.location.city}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left paddingLeft">
+              <i className="fas fa-location-arrow" />
+            </span>
+          </p>
+        </div>
+        <div className="field">
+          <label className="label">Country</label>
+          <p className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="text"
+              name="country"
+              placeholder={userDetails.location.country}
+              value={userDetails.location.country}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left paddingLeft">
+              <i className="fas fa-globe" />
+            </span>
+          </p>
+        </div>
+      </div>
+    );
+  };
 
   const editModeFalse = () => {
     return (
-      <div className="">
-      <span className="icon-text">
-        <span className="icon">
-          <i className="fas fa-envelope" />
+      <div>
+        <span className="icon-text">
+          <span className="icon">
+            <i className="fas fa-envelope" />
+          </span>
+          <span>{userDetails.email}</span>
         </span>
-        <span>{user.email}</span>
-      </span>
-      <br />
-      <span className="icon-text">
-        <span className="icon">
-          <i className="fas fa-location-arrow" />
+        <br />
+        <span className="icon-text">
+          <span className="icon">
+            <i className="fas fa-location-arrow" />
+          </span>
+          <span>{userDetails.location.city}</span>
         </span>
-        <span>{user.location.city}</span>
-      </span>
-      <span className="icon-text">
-        <span className="icon">
-          <i className="fas fa-home" />
+        <span className="icon-text">
+          <span className="icon">
+            <i className="fas fa-home" />
+          </span>
+          <span>
+            {userDetails.location.street.number}{" "}
+            {userDetails.location.street.name}
+          </span>
         </span>
-        <span>{user.location.street.number} {user.location.street.name}</span>
-      </span>
-      <br />
-      <span className="icon-text">
-        <span className="icon">
-          <i className="fas fa-globe" />
+        <br />
+        <span className="icon-text">
+          <span className="icon">
+            <i className="fas fa-globe" />
+          </span>
+          <span>{userDetails.location.country}</span>
         </span>
-        <span>{user.location.country}</span>
-      </span>
-    </div>
-    )
-  } 
+      </div>
+    );
+  };
 
-  useEffect(() => {
-    console.log(editMode)
-  }, [editMode])
-  
-
-   return (
+  return (
     <Fragment>
       <div className="card vc-center ">
         <header className="card-header">
           <p className="card-header-title">
-            {user.name.first} {user.name.last}
+            {userDetails.name.first} {userDetails.name.last}
           </p>
         </header>
         <div className="card-content">
           <div className="content">
-            { editMode ? editModeTrue() : editModeFalse() }
+            {editMode ? editModeTrue() : editModeFalse()}
           </div>
-        <footer className="card-footer">
-            <div className="field is-grouped">
+          <footer className="card-footer">
+            {editMode ? (
               <p className=" card-footer-item">
-              <button className="button is-success" onClick={()=> handleSave()}>
-                <span className="icon ">
-                  <i className="fas fa-check"></i>
-                </span>
-                <span>Save</span>
-              </button>
+                <button
+                  className="button is-success"
+                  onClick={() => handleSave()}
+                >
+                  <span className="icon ">
+                    <i className="fas fa-check"></i>
+                  </span>
+                  <span className="paddingLeft">Save</span>
+                </button>
               </p>
+            ) : (
               <p className=" card-footer-item">
-              <button className="button" onClick={()=> setEditMode(true)}>
-                <span className="icon ">
-                  <i className="fas fa-pen"></i>
-                </span>
-                <span>Edit</span>
-              </button>
+                <button className="button" onClick={() => setEditMode(true)}>
+                  <span className="icon ">
+                    <i className="fas fa-pen"></i>
+                  </span>
+                  <span className="paddingLeft">Edit</span>
+                </button>
               </p>
-            </div>
-        </footer>
+            )}
+          </footer>
+        </div>
       </div>
-    </div>
-  </Fragment>
-  )
-}
+    </Fragment>
+  );
+};
