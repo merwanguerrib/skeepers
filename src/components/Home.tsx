1c0a356e3669c90610a4074808beacb579012de3
Merwan Guerrib <merwan.guerrib@capgemini.com>
2022-03-11 21:30:09 +0100

    Add useCallback to fetchUsers

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,47 +1,42 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import { userType } from '../commonTypes/userType';
-import UserCard from './UserCard';
-const ENDPOINT_RANDOM_USERS = 'https://randomuser.me/api/?results=20';
+import React, { useState, useEffect, Fragment, useCallback } from "react";
+import { userType } from "../commonTypes/userType";
+import UserCard from "./UserCard";
+const ENDPOINT_RANDOM_USERS = "https://randomuser.me/api/?results=20";
 
+function Home(): JSX.Element {
+  const [users, setUsers] = useState<userType[]>([]);
 
-function Home():JSX.Element {
-  const [users, setUsers] = useState<userType[]>([])
-
-  const fetchUsers = async () => {
-    if(users.length === 0){
+  const fetchUsers = useCallback(async () => {
+    if (users.length === 0) {
       try {
-        const response = await fetch(ENDPOINT_RANDOM_USERS)
-        const usersArr = await response.json()
+        const response = await fetch(ENDPOINT_RANDOM_USERS);
+        const usersArr = await response.json();
         const results = usersArr.results.map((result: userType) => {
           let user = {
-          name : result.name,
-          location : result.location,
-          email: result.email
-          } as userType
-          return user
-        })  
-         setUsers(results)
-       } catch (error) {
-         console.error(error)
-       }
+            name: result.name,
+            location: result.location,
+            email: result.email,
+          } as userType;
+          return user;
+        });
+        setUsers(results);
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }
+  }, [users]);
 
   useEffect(() => {
-    fetchUsers()
-      },[users])
+    fetchUsers();
+  }, [fetchUsers, users]);
 
   return (
     <Fragment>
-      {
-       users.map((user:userType,key:number) => { 
-        return(
-        <UserCard {...user} key={key} />
-        )
-      }) 
-      }
+      {users.map((user: userType, key: number) => {
+        return <UserCard {...user} key={key} />;
+      })}
     </Fragment>
-  )
+  );
 }
 
-export default Home
+export default Home;
